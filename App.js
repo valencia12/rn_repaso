129d1e0e0f3d6d1394cff65c6ce8f58d79b1dbbf
Jorge Mirand <00003417@uca.edu.sv>
2021-04-26 19:44:11 -0600

    Agregando text, view, flex dimension and direction

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 
 
-const Texto = () =>{
+const Texto = ({style}) =>{
   /*Para hacer que cambie el texto hay que manderla la funcion como propiedad*/
  /*Se usara el componenete text como componente papa*/
- 
- const [texto, setTexto] = useState("Hola mundo!") 
+ const text1 = "Hola mundo!!"
+ const text2 = "Adios :D"
+
+ const [texto, setTexto] = useState(text1) 
+
  const updateText = () =>{
-  setTexto("Adios :D")
+  setTexto(text2)
+
+  if(texto == text2){
+    setTexto(text1)
+  }
 }
  return(
-    <Text onPress={updateText} >{texto}</Text>
+    <Text style={[styles.Texto, style]} onPress={updateText} >{texto}</Text>
   )
 }
 
@@ -21,18 +27,39 @@ const Texto = () =>{
 export default function App() {
   return (
     <View style={styles.container}>
-     <Texto/>
-    
-      
+     <Texto style={styles.red}/>
+     <Texto style={styles.green}/>
+     <Texto style={styles.blue}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  Texto:{
+
+    fontSize: 25,
+    color: 'white',
+    height: 100,
+    width: 100
+  },
+  red:{ //3 + 2 + 1 = 6
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  green:{
+    flex: 2,
+    backgroundColor: 'green',
+  },
+  blue:{
+    flex: 3,
+    backgroundColor: 'blue',
+  },
   container: {
     flex: 1,
+    flexDirection: 'row-reverse',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
